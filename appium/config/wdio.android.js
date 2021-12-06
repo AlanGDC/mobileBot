f7ef1ca0b2fbd397174c07a11a315f3263a5a8b4
alangdc <nomeafanaronelbuzon@gmail.com>
2021-12-06 14:54:58 -0300

    add browstack url

--- appium/config/wdio.android.js
+++ appium/config/wdio.android.js
@@ -22,11 +22,32 @@ config.capabilities = [
     platformVersion: "8.1",
     automationName: "UiAutomator2",
     deviceName: "Google Pixel",
-    app: process.env.APK
+    // app: process.env.APK
     //"app-release-int-3.apk"
+    project: "First Webdriverio Android Project",
+    build: "browserstack-build-1",
+    name: "local_test",
+    device: "Google Pixel 3",
+    os_version: "9.0",
+    app: process.env.BROWSERSTACK_APP_ID || 'bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c',
+    'browserstack.local': true
           
-  }
+   }
 ];
+// config = {
+//   user: process.env.BROWSERSTACK_USERNAME || 'alanduarte_uyx2Vj',
+//   key: process.env.BROWSERSTACK_ACCESS_KEY || '22AB1Cum9og1kuVnYoyX',
+//   //...
+//   capabilities: [{
+//     project: "First Webdriverio Android Project",
+//     build: "browserstack-build-1",
+//     name: "local_test",
+//     device: "Google Pixel 3",
+//     os_version: "9.0",
+//     app: process.env.BROWSERSTACK_APP_ID || 'bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c',
+//     'browserstack.local': true
+//   }],
+// }
 config.maxInstances = 1;
 config.logLevel = "info"; //trace | debug | info | warn | error | silent
 
